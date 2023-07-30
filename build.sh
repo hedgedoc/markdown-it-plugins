e9bc945d32f3317e976b02382c11618aaf19c8fb
Tilman Vatteroth <git@tilmanvatteroth.de>
2023-07-30 11:44:36 +0200

    fix: replace microbundle with build script that handles hybrid packages better

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 Tilman Vatteroth
+#
+# SPDX-License-Identifier: MIT
+
+
+set -e
+
+echo "Clear dist directory.."
+rm -rf dist
+
+echo "Compile to CJS.."
+tsc --project tsconfig.cjs.json
+
+echo "Compile to ESM.."
+tsc --project tsconfig.esm.json
+
+echo "Fix CJS package.json.."
+cat > dist/cjs/package.json <<!EOF
+{
+    "type": "commonjs"
+}
+!EOF
+
+echo "Fix ESM package.json.."
+cat > dist/esm/package.json <<!EOF
+{
+    "type": "module"
+}
+!EOF
+
+echo "Done!"
